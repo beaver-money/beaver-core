d23e08737057148e8284dab7022f078f9a6f5b0b
Connor Burge <connor.d.burge@gmail.com>
2025-07-01 21:33:01 -0400

    wip

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,13 +1,19 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm"
 import { AccountMembershipsTable } from "@src/db/schema/account-memberships"
 import { AccountsTable } from "@src/db/schema/accounts"
 import { UsersTable } from "@src/db/schema/users"
-import { InferInsertModel, InferSelectModel } from "drizzle-orm"
 
 export type User = InferSelectModel<typeof UsersTable>
 export type NewUser = InferInsertModel<typeof UsersTable>
+export type UpdateUser = Partial<NewUser> & Pick<User, 'id'>
+export type DeleteUser = Pick<User, 'id'>
 
 export type Account = InferSelectModel<typeof AccountsTable>
 export type NewAccount = InferInsertModel<typeof AccountsTable>
+export type UpdateAccount = Partial<NewAccount> & Pick<Account, 'id'>
+export type DeleteAccount = Pick<Account, 'id'>
 
 export type AccountMembership = InferSelectModel<typeof AccountMembershipsTable>
 export type NewAccountMembership = InferInsertModel<typeof AccountMembershipsTable>
+export type UpdateAccountMembership = Partial<Omit<NewAccountMembership, 'accountId' | 'userId'>> & Pick<AccountMembership, 'accountId' | 'userId'>
+export type DeleteAccountMembership = Pick<AccountMembership, 'accountId' | 'userId'>
